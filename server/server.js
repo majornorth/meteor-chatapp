096e7f6ca85408634951a69aa8b783364f2520e4
Stewart McCoy <mccoy.stewart@gmail.com>
2015-03-01 15:11:19 -0800

    show current user's Facebook profile picture

--- server/server.js
+++ server/server.js
@@ -1,23 +1,38 @@
-if (Meteor.isServer) {
-  Meteor.publish('chatMessages', function() {
-    return Messages.find({}, { sort: { time: -1}, limit: 1000});
-  });
+Meteor.publish('chatMessages', function() {
+  return Messages.find({}, { sort: { time: -1}, limit: 1000});
+});
 
-  Meteor.methods({
-    'insertChatMessage': function(name, message) {
-      Messages.insert({
-        name: name,
-        message: message,
-        time: Date.now(),
-      });
+Meteor.methods({
+  'insertChatMessage': function(name, message) {
+    Messages.insert({
+      name: name,
+      message: message,
+      time: Date.now(),
+    });
+  }
+});
+
+var getFbPicture = function(accessToken) { // make async call to grab the picture from facebook
+  var result;
+  result = Meteor.http.get("https://graph.facebook.com/me", {
+    params: {
+      access_token: accessToken,
+      fields: 'picture'
     }
   });
+  if(result.error) {
+    throw result.error;
+  }
+  return result.data.picture.data.url; // return the picture's url
+};
 
-  Accounts.onCreateUser(function(options, user) {
-      if (!options.profile) {
-        user.profile = {name: 'Anonymous'};
-      }
-      return user;
-    });
-}
+Accounts.onCreateUser(function(options, user) {
+  if (!options.profile) {
+    user.profile = {name: 'Anonymous'};
+  } else {
+    options.profile.picture = getFbPicture(user.services.facebook.accessToken);
+    user.profile = options.profile; // We still want the default 'profile' behavior.
+  }
 
+  return user;
+});
